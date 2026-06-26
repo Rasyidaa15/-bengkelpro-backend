501e78e43ca8d0574cd66417c03a4c9e143d9ff3
Rasyida Messy <rasyidamessy@gmail.com>
2026-06-26 12:20:19 +0700

    Make product endpoint public

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,16 +6,18 @@ const {
   updateProduct,
   deleteProduct,
 } = require('../controllers/productController');
+
 const { protect, admin } = require('../middleware/auth');
+
 const router = express.Router();
 
-router.route('/')
-  .get(protect, getProducts)
-  .post(protect, admin, createProduct);
+// PUBLIC
+router.get('/', getProducts);
+router.get('/:id', getProductById);
 
-router.route('/:id')
-  .get(protect, getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+// ADMIN
+router.post('/', protect, admin, createProduct);
+router.put('/:id', protect, admin, updateProduct);
+router.delete('/:id', protect, admin, deleteProduct);
 
 module.exports = router;
