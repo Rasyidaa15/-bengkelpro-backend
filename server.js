ca074592e16b580c0ba6d5d71e703fd6937c021b
Rasyida Messy <rasyidamessy@gmail.com>
2026-06-26 11:00:35 +0700

    fix: use authenticate instead of sync

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-sequelize.sync({ alter: false }).then(() => {
+sequelize.authenticate().then(() => {
   app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
     console.log(`✅ Database: ${process.env.DB_NAME}`);
